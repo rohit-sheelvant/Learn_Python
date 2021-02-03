c019315c068d51d22098818620553eee21219dbf
rohit-sheelvant <54717356+rohit-sheelvant@users.noreply.github.com>
2021-02-03 14:17:32 +0530

    calculating BMI of a person

---
+++ Day-2-2.py
@@ -0,0 +1,8 @@
+# calculating BMI of a person
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+h = float(height)
+w = float(weight)
+bmi = (w / (h * h))
+print(int(bmi))
